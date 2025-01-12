8a05b3c95daea9d62c49bbd928269241d8066cc6
Duduumendez <nicksoniaudax5@gmail.com>
2025-01-13 02:42:02 +0300

    Update social_handler.py

--- handlers/social_handler.py
+++ handlers/social_handler.py
@@ -9,9 +9,18 @@ def handle_social(bot, message, url):
 
     try:
         # Download video
-        ydl_opts = {'outtmpl': f'{video_path}/%(title)s.%(ext)s'}
+        ydl_opts = {
+            'outtmpl': f'{video_path}/%(title)s.%(ext)s',
+            'format': 'best',
+        }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
+            file_path = ydl.prepare_filename(info)
             bot.send_message(message.chat.id, f"Downloaded: {info['title']}")
+
+        # Send video back to the user
+        with open(file_path, 'rb') as video:
+            bot.send_video(message.chat.id, video)
+
     except Exception as e:
         bot.reply_to(message, f"Error downloading video: {e}")
